14689ce38eeac9dcaa3d9ce5458c799d8b40e277
forest-jpg <forest.soil0922@gmail.com>
2026-02-22 21:31:02 +0900

    新規登録　CMake学習用

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, s, n) for (int i = s; i < (int)(n); i++)
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
